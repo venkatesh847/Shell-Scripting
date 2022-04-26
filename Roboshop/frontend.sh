a37aef8c87b12427bf1733a7019bbcdb7e39aca8
raju <isnraju462@gmail.com>
2022-04-26 12:23:58 +0530

    variable

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
-
-LOG=/tmp/roboshop.log
-rm -f $LOG
+source common.sh
 echo -n -e "Installing nginx\t\t.."
 yum install nginx -y &>>$LOG
-if [ $? -eq 0 ]; then
-  echo "done"
-  else
-    echo "fail"
-    exit 1
- fi
+STAT_CHECK $?
 echo -n -e "Enabling Nginx\t\t.."
 systemctl enable nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo "done"
-  else
-    echo "fail"
-    exit 1
- fi
+STAT_CHECK $?
 echo -n -e "Starting Ngin\t\t.."
 systemctl start nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo "done"
-  else
-    echo "fail"
-    exit 1
- fi
+STAT_CHECK $?
