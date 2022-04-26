b85f28372c5b391dcc54d4ac08922aaa2f0efdc8
raju <isnraju462@gmail.com>
2022-04-26 10:19:30 +0530

    variable

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -4,6 +4,6 @@ LOG=/tmp/roboshop.log
 echo -e "Installing nginx\t\t......\t\e[32mdone\e[0m"
 yum install nginx -y > $LOG
 echo "Enabling Nginx"
-systemctl enable nginx
+systemctl enable nginx > $LOG
 echo "Starting Nginx"
-systemctl start nginx
+systemctl start nginx > $LOG
