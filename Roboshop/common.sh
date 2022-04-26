19e59b37eae4ad119c214ef297cdd73332116776
raju <isnraju462@gmail.com>
2022-04-26 12:30:51 +0530

    variable

--- Roboshop/common.sh
+++ Roboshop/common.sh
@@ -1,3 +1,9 @@
+User_id=$(id -u)
+if [$User_id -ne 0]; then
+  echo "you should be a root"
+  exit 2
+  fi
+
 STAT_CHECK() {
 if [ $1 -eq 0 ]; then
   echo "done"
@@ -8,3 +14,4 @@ if [ $1 -eq 0 ]; then
  }
 LOG=/tmp/roboshop.log
 rm -f $LOG
+
